00ac0161cfffb9a2736256630cf463a55c3cd720
Syeda Fatima Ahmad <fatima.ahmad6543@gmail.com>
2025-11-28 09:49:46 +0500

    update

--- src/components/customer/Navbar.jsx
+++ src/components/customer/Navbar.jsx
@@ -68,7 +68,7 @@ export default function Navbar() {
   return (
     <nav
       ref={navRef}
-      className="fixed top-0 left-0 w-full text-white px-10 py-3 flex items-center justify-between z-50  bg-black/80 md:bg-black/10"
+      className="fixed top-0 left-0 w-full text-white px-10 py-3 flex items-center justify-between z-50 bg-gradient-to-r from-black to-black/30"
       
     >
 
