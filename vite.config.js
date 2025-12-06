ca1adb1a67d01d6b03b0672f66f18e8c3e899bb2
Syeda Fatima Ahmad <fatima.ahmad6543@gmail.com>
2025-12-06 19:15:05 +0500

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
-    base: '/ahsan-labs/'
+    base: '/ahsan-labs'
   }
 })
