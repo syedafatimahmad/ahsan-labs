cc0c9a9c641cac8eb63f2822607d09cc2949fafb
Syeda Fatima Ahmad <fatima.ahmad6543@gmail.com>
2025-12-07 00:08:47 +0500

    update

--- src/components/customer/Button.jsx
+++ src/components/customer/Button.jsx
@@ -1,8 +1,10 @@
-export default function Button({ children, onClick, className }) {
+export default function Button({ children, onClick, className = "", type = "button", ...props }) {
   return (
     <button
+      type={type}
       onClick={onClick}
       className={`px-8 py-3 bg-gradient-to-r from-blue-600 to-gray-900 transition ${className}`}
+      {...props}
     >
       {children}
     </button>
